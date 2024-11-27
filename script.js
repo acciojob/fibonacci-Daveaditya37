92abe4b955480fa8a86d363f94923896025a2113
Daveaditya37 <160773920+Daveaditya37@users.noreply.github.com>
2024-11-27 09:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function fibonacci(num-1) {
+function fibonacci(num+1) {
 // your code here
-	if(num-1<2){
-		return num-1;
+	if(num+1<2){
+		return num+1;
 	}
 	else{
-		return fibonacci((num-1)-1)+ fibonacci((num-1)-2);
+		return fibonacci((num+1)-1)+ fibonacci((num+1)-2);
 	}
 }
 
