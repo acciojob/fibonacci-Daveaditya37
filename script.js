c9ec0ef2fb1d35bbe95b2973976ffe5e9d058697
Daveaditya37 <160773920+Daveaditya37@users.noreply.github.com>
2024-11-27 10:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function fibonacci(num+1) {
-// your code here
-	if(num+1<2){
-		return num+1;
-	}
-	else{
-		return fibonacci((num+1)-1)+ fibonacci((num+1)-2);
-	}
+function fibonacci(num) {
+    // Base cases
+    if (num === 1) {
+        return 0; // First Fibonacci number
+    }
+    if (num === 2) {
+        return 1; // Second Fibonacci number
+    }
+    // Recursive case
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
